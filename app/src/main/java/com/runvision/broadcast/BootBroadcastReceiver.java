32261bf5a33ebbf63c4b5318b3713e23431b9d13
lichao3140 <396229938@qq.com>
2018-10-21 10:28:24 +0800

    集成

--- app/src/main/java/com/runvision/broadcast/BootBroadcastReceiver.java
+++ app/src/main/java/com/runvision/broadcast/BootBroadcastReceiver.java
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.runvision.g68a_sn.LoginActivity;
 import com.runvision.g68a_sn.MainActivity;
 
 public class BootBroadcastReceiver extends BroadcastReceiver {
@@ -12,9 +13,8 @@ public class BootBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO Auto-generated method stub
         if (intent.getAction().equals(ACTION)) {
-            Intent mainActivityIntent = new Intent(context, MainActivity.class);
+            Intent mainActivityIntent = new Intent(context, LoginActivity.class);
             mainActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(mainActivityIntent);
         }
